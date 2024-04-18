ff6c7120fbc06fa8b2fb62197965bfa156e3ca72
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2024-04-18 10:58:44 +0800

    FEAT: roll dice button

---
+++ README.md
@@ -0,0 +1,3 @@
+# yahtzee
+
+Mimic the original [YAHTZEE With Buddies Dice Game](https://play.google.com/store/apps/details?id=com.scopely.yux&pcampaignid=web_share)
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:yahtzee/yahtzee.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //  root of this application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: YahtzeeGamePage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
